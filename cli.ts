ba895f902b11fb0228be7fd12cc4ed15599050e4
Jacob Hummer <jcbhmr@outlook.com>
2023-05-28 22:52:17 -0500

    Handle those inputst

--- cli.ts
+++ cli.ts
@@ -5,70 +5,82 @@
  * @file
  */
 
-import parseBoolean from "npm:parseboolean";
-import { $ } from "npm:zx";
-import { isFunction, isRegExp } from "npm:is-what";
+import process from "node:process";
+import { readFile, writeFile } from "node:fs/promises";
+import assert from "node:assert";
+import * as core from "npm:@actions/core@^1.10.0";
+import { $ } from "npm:zx@^7.2.2";
 
-const runnerDebug = parseBoolean(Deno.env.get("RUNNER_DEBUG")!);
-$.verbose = runnerDebug;
+globalThis.addEventListener("error", (e) => core.setFailed(e.error.message));
 
-const input = JSON.parse(Deno.env.get("INPUT")!) as Record<string, string>;
-const token = input.token;
-const githubServerURL = new URL(input["github-server-url"]);
-const dryRun = parseBoolean(input["dry-run"]);
-const query = input.query;
-const secrets = JSON.parse(input.secrets) as Record<string, string>;
-const secretFilterRaw = (0, eval)(input["secret-filter"]);
-const secretFilter: (k: string, v: string) => boolean = isFunction(
-  secretFilterRaw
-)
-  ? (secretFilterRaw as (k: string, v: string) => boolean)
-  : isRegExp(secretFilterRaw)
-  ? (k: string) => secretFilterRaw.test(k)
-  : () => true;
+$.verbose = false;
+if (core.isDebug()) {
+  $.verbose = true;
+}
 
-Deno.env.set("GITHUB_TOKEN", token);
-Deno.env.set("GH_HOST", githubServerURL.host);
+async function findAllRepositories(query: string): Promise<string> {
+  return (
+    await $`gh search repos -L 500 ${query} --json fullName --jq .[].fullName`
+  )
+    .toString()
+    .trim();
+}
+
+const token = core.getInput("token", { required: true });
+const githubServerURL = core.getInput("github-server-url", { required: true });
+process.env.GITHUB_TOKEN = token;
+process.env.GITHUB_SERVER_URL = githubServerURL;
+
+const dryRun = core.getBooleanInput("dry-run", { required: true });
+
+let repositories = core.getInput("repositories");
+const repository = core.getInput("repository");
+const query = core.getInput("query");
+assert(
+  repositories || repository || query,
+  "Must provide either repositories or query"
+);
+repositories ||= repository;
+repositories ||= await findAllRepositories(query);
+core.startGroup("repositories");
+core.debug(repositories);
+core.endGroup();
+
+const app = core.getInput("app", { required: true });
+assert(
+  ["actions", "codespaces", "dependabot"].includes(app),
+  "app must be {actions|codespaces|dependabot}"
+);
+
+let secrets = core.getInput("secrets");
+const secret = core.getInput("secret");
+assert(secrets || secret, "Must provide either secrets or secret");
+secrets ||= secret;
+core.startGroup("secrets");
+core.debug(secrets);
+core.endGroup();
 
 const envFile = await Deno.makeTempFile({ suffix: ".env" });
 globalThis.addEventListener("unload", () => Deno.removeSync(envFile));
-
-for (const [k, v] of Object.entries(secrets)) {
-  if (
-    !/^github/i.test(k) &&
-    !/^actions/i.test(k) &&
-    v !== token &&
-    secretFilter(k, v)
-  ) {
-    await Deno.writeTextFile(envFile, `${k}=${v}\n`, { append: true });
-  } else if (runnerDebug) {
-    console.info(`Skipping ${k}`);
-  }
-}
-if (runnerDebug) {
-  console.info(await Deno.readTextFile(envFile));
+await writeFile(envFile, secrets);
+if (core.isDebug()) {
+  core.startGroup(envFile);
+  core.debug(await readFile(envFile, "utf8"));
+  core.endGroup();
 }
 
-const repositories = (
-  "" + (await $`gh search repos ${query} --json fullName --jq .[].fullName`)
-)
-  .trim()
-  .split(/\s+/g)
-  .filter((s) => s !== Deno.env.get("GITHUB_REPOSITORY")!);
-
-if (!repositories.length) {
-  console.warn("No repositories found");
-}
-const errors: Error[] = [];
-for (const repository of repositories) {
-  if (dryRun) {
-    console.info`gh secret set -R ${repository} -f ${envFile}`;
-  } else {
-    await $`gh secret set -R ${repository} -f ${envFile}`.catch((e) =>
-      errors.push(e)
-    );
-  }
-}
-if (errors.length) {
-  throw new AggregateError(errors);
+const results = await Promise.allSettled(
+  repositories
+    .split(/\r?\n/g)
+    .map((r) =>
+      dryRun
+        ? $`gh secret set -R ${r} -a ${app} -f ${envFile}`
+        : core.info(`gh secret set -R ${r} -a ${app} -f ${envFile}`)
+    )
+);
+const failed = results.filter(
+  (r): r is PromiseRejectedResult => r.status === "rejected"
+);
+if (failed.length) {
+  throw new AggregateError(failed.map((r) => r.reason));
 }
